ca55c79a3a5367c0ca6c1d2f65125d76462525ec
Nick <neatomaru@gmail.com>
2024-04-17 23:22:20 +0300

    add skeleton

---
+++ src/main/java/me/neatomaru/monitoringyandexapp/MonitoringYandexAppApplication.java
@@ -0,0 +1,13 @@
+package me.neatomaru.monitoringyandexapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class    MonitoringYandexAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonitoringYandexAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/neatomaru/monitoringyandexapp/MonitoringYandexAppApplicationTests.java
@@ -0,0 +1,13 @@
+package me.neatomaru.monitoringyandexapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoringYandexAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
